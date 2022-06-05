3d6cd2b6771161268ac76ec602ccdcff86e4c07b
sharathguvvala <guvvalasharath@gmail.com>
2022-06-05 23:00:25 +0530

    changed network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require('@nomiclabs/hardhat-waffle')
 
-require('dotenv').config({path:".env"})
+require('dotenv').config()
 
 const Infura_API_Key = process.env.Infura_API_Key
 const Goerli_Private_Key = process.env.Goerli_Private_Key
